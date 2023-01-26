7ada326d3868503116f51d6209e7176b54614952
André Bessa <andrebessax@gmail.com>
2023-01-26 16:05:44 -0300

    Update: Inicio da definição do modelo de classes, criado no projeto a classe Conta.

---
+++ bytebank_API/Domain/Class1.cs
@@ -0,0 +1,6 @@
+
+namespace Domain;
+public class Class1
+{
+
+}
---
+++ bytebank_API/bytebank_API/Conta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bytebank_API
+{
+    public class Conta
+    {
+        public int IdConta { get; set; }
+        public string NumeroConta { get; set; }
+        public Cliente Titular { get; set; }
+        public double Saldo { get; set; }
+        public Agencia Agencia { get; set; }
+        public string ChavePix { get; set; }
+        public TipoConta Tipo { get; set; }
+
+        public void Sacar()
+        {
+
+        }
+        public void Transferir()
+        {
+
+        }
+        public void Depositar()
+        {
+
+        }
+        public void Rendimento()
+        {
+
+        }
+
+    }
+}
