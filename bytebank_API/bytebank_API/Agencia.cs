fef5a6553bfcbb07556b8f5a71c8b196cccccc8e
Enzo Gabriel <thecitendogames@gmail.com>
2023-01-30 21:58:43 -0300

    Criando Classes
    As classes Agencia, Cliente,Endereco,EnderecoAgencia,EnderecoCliente e Enum foram criadas com a suas devidas propriedas

---
+++ bytebank_API/bytebank_API/Agencia.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bytebank_API
+{
+    public class Agencia
+    {
+        public int IdAgencia { get; set; }
+        public int NumeroAgencia { get; set; }
+        public string NomeAgencia { get; set; }
+        public EnderecoAgencia EnderecoAgencia { get; set; }
+    }
+}
---
+++ bytebank_API/bytebank_API/Endereco.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bytebank_API
+{
+    public abstract class Endereco
+    {
+        public int IdEndereco { get; set; }
+        public string Logradouro { get; set; } 
+        public string Complemento { get; set; }
+        public int Numero { get; set; }
+        public string CEP { get; set; }
+    }
+}
