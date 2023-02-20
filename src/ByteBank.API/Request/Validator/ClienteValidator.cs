33519fa9ba748b8ea6f060288ba056a9cdb76d91
Walter <silvawalteralipio@gmail.com>
2023-02-20 19:25:33 -0300

    Add: validações nas classes ClienteValidator e ContaValidator

--- src/ByteBank.API/Request/Validator/ClienteValidator.cs
+++ src/ByteBank.API/Request/Validator/ClienteValidator.cs
@@ -7,6 +7,24 @@ public class ClienteValidator : AbstractValidator<ClienteRequest>
 {
     public ClienteValidator()
     {
+        RuleFor(c => c.Cpf)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty().WithMessage("CPF é obrigatório")
+            .Matches(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$").WithMessage("Informe o CPF no formato 000.000.000-00");
 
+        RuleFor(c => c.Nome)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty().WithMessage("Nome é obrigatório")
+            .Length(3, 30).WithMessage("Mínimo de 3 caracteres, máximo de 30 caracteres");
+        RuleFor(c => c.Tipo)
+            .Cascade(CascadeMode.Stop)
+            .NotNull().WithMessage("Tipo cliente é obrigatório")
+            .IsInEnum().WithMessage("Expecifique 0 - PessoaFísica, 1 - Cnpj");
+
+        RuleForEach(c => c.Contas)
+            .SetValidator(new ContaValidator());
+
+        RuleFor(c => c.Endereco)
+            .SetValidator(new EnderecoValidator());
     }
 }
